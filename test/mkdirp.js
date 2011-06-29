f618b3bf854ece3941357827e31cbc5cc41213c6
James Halliday <mail@substack.net>
2011-06-29 11:20:43 -0700

    never called back failing test

--- test/mkdirp.js
+++ test/mkdirp.js
@@ -9,6 +9,9 @@ exports.woo = function () {
     var z = Math.floor(Math.random() * Math.pow(16,4)).toString(16);
     
     var file = '/tmp/' + [x,y,z].join('/');
+    var to = setTimeout(function () {
+        assert.fail('never called back');
+    }, 1000);
     
     mkdirp(file, 0755, function (err) {
         if (err) assert.fail(err);
@@ -17,6 +20,7 @@ exports.woo = function () {
             else fs.stat(file, function (err, stat) {
                 if (err) assert.fail(err)
                 else {
+                    clearTimeout(to);
                     assert.eql(stat.mode & 0777, 0755);
                     assert.ok(stat.isDirectory(), 'target not a directory');
                 }
