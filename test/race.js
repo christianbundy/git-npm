48e67fce39f1a5f2aba6196301b7be6d4545d6cf
isaacs <i@izs.me>
2015-05-13 14:47:28 -0700

    Remove octal literals, pass in strict mode
    We all hate this.  But since this module is used everywhere, it's
    a pain to not be able to use it in environments where strict mode
    is desired, even if that desire is misguided.

--- test/race.js
+++ test/race.js
@@ -3,6 +3,8 @@ var path = require('path');
 var fs = require('fs');
 var exists = fs.exists || path.exists;
 var test = require('tap').test;
+var _0777 = parseInt('0777', 8);
+var _0755 = parseInt('0755', 8);
 
 test('race', function (t) {
     t.plan(10);
@@ -20,13 +22,13 @@ test('race', function (t) {
     mk(file);
     
     function mk (file, cb) {
-        mkdirp(file, 0755, function (err) {
+        mkdirp(file, _0755, function (err) {
             t.ifError(err);
             exists(file, function (ex) {
                 t.ok(ex, 'file created');
                 fs.stat(file, function (err, stat) {
                     t.ifError(err);
-                    t.equal(stat.mode & 0777, 0755);
+                    t.equal(stat.mode & _0777, _0755);
                     t.ok(stat.isDirectory(), 'target not a directory');
                 });
             })
